fb3be7a4f20d70af2620839e0abc05b8c6263a3e
satya-supercluster <satya.supercluster@gmail.com>
2025-01-12 19:27:00 +0530

    Fix: Increase transition duration in ScrollToTop component for smoother animation

--- frontend/src/components/ScrollToTop/ScrollToTop.jsx
+++ frontend/src/components/ScrollToTop/ScrollToTop.jsx
@@ -14,7 +14,7 @@ const ScrollToTop = () => {
       <motion.div
         initial={{ y: 50 }}
         animate={{ y: 0 }}
-        transition={{ duration: 0.3}}
+        transition={{ duration: 0.8}}
         onClick={scrollUp}
         className="fixed bottom-2 right-5 w-10 h-10 flex justify-center items-center rounded-2xl text-white text-xl bg-primary shadow-[0_1px_2px_1px_rgba(0,0,0,0.6)] cursor-pointer"
       >
