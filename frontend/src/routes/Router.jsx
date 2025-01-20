ffaf47a189211aff2ca9faceb69c29f5c96d1319
satya-supercluster <satya.supercluster@gmail.com>
2025-01-21 00:33:35 +0530

    fix: replace NotFound component with Navigate for improved routing

--- frontend/src/routes/Router.jsx
+++ frontend/src/routes/Router.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 
 import About from "@/pages/About";
 import Calculator from "@/pages/Calculator";
@@ -6,20 +6,20 @@ import Contacts from "@/pages/Contacts";
 import Disclaimer from "@/pages/Disclaimer";
 import Home from "@/pages/Home";
 import Layout from "@/layout/Layout";
-import NotFound from "@/pages/NotFound";
+// import NotFound from "@/pages/NotFound";
 import Services from "@/pages/Services";
 
 const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<Home />}/>
-        <Route path="/about" element={<About />}/>
-        <Route path="/services" element={<Services />}/>
-        <Route path="/calculator" element={<Calculator />}/>
-        <Route path="/contacts" element={<Contacts />}/>
-        <Route path="/disclaimer" element={<Disclaimer />}/>
-        <Route path="*" element={<NotFound />}/>
+        <Route index element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/calculator" element={<Calculator />} />
+        <Route path="/contacts" element={<Contacts />} />
+        <Route path="/disclaimer" element={<Disclaimer />} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Route>
     </Routes>
   );
