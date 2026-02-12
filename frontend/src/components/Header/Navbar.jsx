c26133d623fb065c947d95bab9c09396cd89306c
Gaurang Tandon <1gaurangtandon@gmail.com>
2026-02-12 19:35:53 +0530

    Update link

--- frontend/src/components/Header/Navbar.jsx
+++ frontend/src/components/Header/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
     { title: "Services", path: "/services" },
     { title: "Calculator", path: "/calculator" },
     { title: "Contact", path: "/contacts" },
-    { title: "Login", path: "https://prarambhcapitalservices.themfbox.com/" },
+    { title: "Login", path: "https://aarnikcapital.themfbox.com/" },
   ];
 
   const handleSidebarClose = () => setIsSidebarOpen(false);
